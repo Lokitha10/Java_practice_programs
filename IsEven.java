9c9dd350a56ffe20c6ab690e09bd6d7cb7f14f5d
Thathireddy Lokitha <lokitha7814@gmail.com>
2025-08-19 22:40:51 +0530

    Update IsEven.java

--- IsEven.java
+++ IsEven.java
@@ -1,3 +1,5 @@
+//Write a function to find whether the given input number is Even.If the given number is even, the function should return 2 else it should return 1.
+
 import java.io.*;
 import java.util.*;
 class UserMainCode{
