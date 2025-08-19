2fad68e48c7c7f6c6b3466095ad013d2b9d7daed
Thathireddy Lokitha <lokitha7814@gmail.com>
2025-08-19 22:42:24 +0530

    Update IsOdd.java

--- IsOdd.java
+++ IsOdd.java
@@ -1,3 +1,5 @@
+//Write a function to find whether the given input number is Odd.If the given number is Odd, the function should return 2 else it should return 1.
+
 import java.io.*;
 import java.util.*;
 class UserMainCode{
