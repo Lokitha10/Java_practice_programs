a13f2a7be0b3a3f2c8ed1617a6f81baa5a04c021
Thathireddy Lokitha <lokitha7814@gmail.com>
2025-08-23 21:56:18 +0530

    Create IsOdd.java

---
+++ numbers_programs/IsOdd.java
@@ -0,0 +1,10 @@
+//Write a function to find whether the given input number is Odd.If the given number is Odd, the function should return 2 else it should return 1.
+
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+  public int isOdd(int input1){
+    if(input1%2!=0) return 2;
+      else return 1;
+  }
+}
